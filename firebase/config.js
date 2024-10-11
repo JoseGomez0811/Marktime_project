7886b1be67a0fa63b66b4a4606c6fd999480ed2c
JoseGomez0811 <gomez.jose@correo.unimet.edu.ve>
2024-10-11 13:02:45 -0400

    Configuración con fireabse

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBD29gfHstZxPHMCZwYz27H3N3D69GkXFo",
+  authDomain: "db-marktime.firebaseapp.com",
+  projectId: "db-marktime",
+  storageBucket: "db-marktime.appspot.com",
+  messagingSenderId: "907550575420",
+  appId: "1:907550575420:web:fa8c5f6333348f82faec39",
+  measurementId: "G-L7BBMYTHM9"
+};
+
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
