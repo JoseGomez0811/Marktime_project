8d47d2091b9fc6dd5378e2692d565a9c715eac9c
tochirito <eros.vilaroel@gmail.com>
2024-10-21 18:47:58 -0400

    wtf ta pasando

--- firebase/users-service.js
+++ firebase/users-service.js
@@ -1,6 +1,6 @@
 import { doc, setDoc, updateDoc } from "firebase/firestore"; // Asegúrate de importar updateDoc
 import { db } from "./config";
-import { collection, addDoc, query, where, getDocs } from "firebase/firestore"; 
+import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 
 const USERS_COLLECTION = "Registro-Empleados"; // Define la colección de usuarios
 
@@ -16,7 +16,7 @@ export const insertData = async (formData) => {
       Cargo: formData.cargo,
       Banco: [formData.banco, parseInt(formData.cuenta)],
       Sueldo: parseFloat(formData.sueldo),
-      Password: formData.password // Asegúrate de manejar las contraseñas de forma segura
+      Password: formData.password, // Asegúrate de manejar las contraseñas de forma segura
     });
     console.log("Documento agregado con éxito");
   } catch (error) {
@@ -40,7 +40,7 @@ export async function getUserProfile(correo) {
     if (results.size > 0) {
       const [user] = results.docs.map((item) => ({
         ...item.data(), // Trae todos los datos del documento
-        id: item.id,    // Incluye el ID del documento
+        id: item.id, // Incluye el ID del documento
       }));
       return user;
     } else {
