e7ff02f8c52fd7fe0447a8864a3b8cbd45cce737
CristianF4zio <131079174+CristianF4zio@users.noreply.github.com>
2024-10-25 17:37:32 -0400

    diseño y colores

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -17,58 +17,60 @@ export const Sidebar = () => {
     try {
       await logout(); // Llama la función logout de tu servicio
       navigate(LOGIN_URL); // Redirige al login después de cerrar sesión
-      navigate(LOGIN_URL); // Redirige al login después de cerrar sesión
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
     }
   };
 
   return (
-    <aside className={styles.sidebar}> {/* Usa la clase de estilo correcta */}
-      <div className={styles.sidebarHeader}>
-        <img src="/Marktime-logo-2.svg" alt="Logo de la empresa" className={styles.companyLogo} />
-        <h1 className={styles.companyName}>Marktime</h1>
+    <aside > {/* Usa la clase de estilo correcta */}
+      <div className={styles.sidebar}>
+        <div className={styles.sidebarHeader}>
+          <img src="../public/Marktime-logo-2.svg" alt="Logo de la empresa" className={styles.companyLogo} />
+          <h1 className={styles.companyName}>Marktime</h1>
+        </div>
+
+        <div className={styles.userInfo}>
+          <span className={styles.userName}>
+            {user?.Nombres + ' ' + user?.Apellidos || user?.displayName || "Usuario Anónimo"}
+          </span>
+        </div>
+
+        <nav className={styles.sidebarNav}>
+          <Link to={PROFILE_URL} className={styles.navItem}>
+            <User size={20} />
+            <span>Perfil</span>
+          </Link>
+
+          {/* Renderiza según el tipo de usuario */}
+          {user?.Cargo === 'Empleado' && (
+            <Link to={TRACKING_URL} className={styles.navItem}>
+              <Activity size={20} />
+              <span>Tracking</span>
+            </Link>
+          )}
+
+          {(user?.Cargo === 'Empleador' || user?.Cargo === 'Recursos Humanos') && (
+            <Link to={LIST_URL} className={styles.navItem}>
+              <Users size={20} />
+              <span>Listado de Empleados</span>
+            </Link>
+          )}
+
+          {user?.Cargo === 'Recursos Humanos' && (
+            <Link to={REGISTER_URL} className={styles.navItem}>
+              <UserPlus size={20} />
+              <span>Registrar Empleado</span>
+            </Link>
+          )}
+        </nav>
+
+        <button onClick={handleLogout} className={styles.logoutButton}>
+          <LogOut size={20} />
+          <span>Cerrar Sesión</span>
+        </button>
       </div>
-
-      <div className={styles.userInfo}>
-        <span className={styles.userName}>
-          {user?.Nombres + ' ' + user?.Apellidos || user?.displayName || "Usuario Anónimo"}
-        </span>
-      </div>
-
-      <nav className={styles.sidebarNav}>
-        <Link to="/profile" className={styles.navItem}>
-          <User size={20} />
-          <span>Perfil</span>
-        </Link>
-
-        {/* Renderiza según el tipo de usuario */}
-        {user?.Cargo === 'Empleado' && (
-          <Link to={TRACKING_URL} className={styles.navItem}>
-            <Activity size={20} />
-            <span>Tracking</span>
-          </Link>
-        )}
-
-        {(user?.Cargo === 'Empleador' || user?.Cargo === 'Recursos Humanos') && (
-          <Link to="/employees" className={styles.navItem}>
-            <Users size={20} />
-            <span>Listado de Empleados</span>
-          </Link>
-        )}
-
-        {user?.Cargo === 'Recursos Humanos' && (
-          <Link to={REGISTER_URL} className={styles.navItem}>
-            <UserPlus size={20} />
-            <span>Registrar Empleado</span>
-          </Link>
-        )}
-      </nav>
-
-      <button onClick={handleLogout} className={styles.logoutButton}>
-        <LogOut size={20} />
-        <span>Cerrar Sesión</span>
-      </button>
+      
     </aside>
   );
 };
