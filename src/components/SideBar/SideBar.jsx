bc7d13e3f9de66edbc473b1e56a064cb039d6b47
José Gómez <gomez.jose@correo.unimet.edu.ve>
2024-11-25 15:10:12 -0400

    commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,16 +2,86 @@ import { Link, useNavigate } from "react-router-dom";
 import styles from "./SideBar.module.css"; // Asegúrate de que los estilos estén definidos
 import { useUserContext } from "../../contexts/UserContext";
 import { logout } from "../../../firebase/auth-service";
-import React, { useState } from "react";
-import { LogOut, User, Users, UserPlus, Activity } from "lucide-react";
+import React, { useState, useEffect } from "react";
+import { LogOut, User, Users, UserPlus, Activity, User2 } from "lucide-react";
 import { TRACKING_URL, REGISTER_URL, LOGIN_URL, PROFILE_URL, LIST_URL } from "../../constants/urls";
+//import { test } from "../../pages/Tracking/Tracking"
+import { doc, updateDoc, collection, query, where, getDocs, writeBatch } from 'firebase/firestore';
+import { db } from '../../../firebase/config';
+import { updateEmployeeStatus } from "../../../firebase/users-service";
+
+const ConfirmBox = ({ onConfirm, onCancel }) => (
+  <div className={styles.overlayConfirmBox}>
+    {" "}
+    <div className={styles.confirmBox}>
+      {" "}
+      <p>¿Si cambias de ventana, perderas el registro de las horas trabajadas, primero debes detener el contador?</p>{" "}
+      <button className={styles.stopButton2} onClick={onCancel}>
+        Ok
+      </button>{" "}
+    </div>{" "}
+  </div>
+);
 
 export const Sidebar = () => {
   const { user } = useUserContext(); // Traemos el contexto de usuario
   const navigate = useNavigate(); // Para redirigir después del logout
   const [status, setStatus] = useState("Desconectado");
+  const [showConfirm, setShowConfirm] = useState(false);
   
   console.log('Usuario:', user);
+
+
+  const fetchUsers = async () => {
+    try {
+      try {
+        const userQuery = query(
+          collection(db, "Registro-Empleados"),
+          where("Correo", "==", user.Correo)
+        );
+    
+        const results = await getDocs(userQuery);
+    
+        if (results.size > 0) {
+          const [user2] = results.docs.map((item) => ({
+            ...item.data(),
+            id: item.id,
+          }));
+          return user2.Status;
+        } else {
+          console.log("No se encontró ningún usuario con el correo proporcionado");
+          return null;
+        }
+      } catch (error) {
+        console.error("Error al obtener el perfil del usuario: ", error);
+        throw error;
+      }
+    } catch (error) {
+        console.error("Error al obtener usuarios:", error);
+    }
+};
+
+useEffect(() => {
+  fetchUsers();
+  const intervalId = setInterval(() => {
+      fetchUsers();
+  }, 500);
+  return () => clearInterval(intervalId);
+}, []);
+  
+  const handleStop = async () => {
+    console.log(user.Cargo)
+    const status = await fetchUsers();
+    console.log(status)
+    if(user.Cargo === "Empleado" && status === "Trabajando"){
+      setShowConfirm(true);
+    }
+  };
+
+  const handleCancel = () => {
+    setShowConfirm(false);
+    navigate(TRACKING_URL);
+  };
 
   // Maneja el cierre de sesión
   const handleLogout = async () => {
@@ -27,6 +97,11 @@ export const Sidebar = () => {
 
   return (
     <aside >
+      {showConfirm && (
+        <ConfirmBox
+          onCancel={handleCancel}
+        />
+      )}
       <div className={styles.sidebar}>
         <div className={styles.sidebarHeader}>
           <img src="../public/download.svg" alt="Logo de la empresa" className={styles.companyLogo}/>
@@ -39,7 +114,8 @@ export const Sidebar = () => {
           </span>
         </div>
 
-        <nav className={styles.sidebarNav}>
+        <nav className={styles.sidebarNav} onClick={handleStop}>
+          
           <Link to={PROFILE_URL} className={styles.navItem}>
             <User size={20} className={styles.userIcon}/>
             <span>Perfil</span>
